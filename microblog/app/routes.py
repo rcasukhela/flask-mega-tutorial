e2eea678e4a51a1c2361abd7984978a8804aad8c
Rohan Casukhela <43920866+rcasukhela@users.noreply.github.com>
2023-05-30 15:55:14 -0400

    Understanding Jinja2 Template Engine
    Jinja2: control flow, if-else, for loops, variables.

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,20 +1,31 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' +user['username']+'''.</h1>
-    </body>
-</html>
-'''
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    # We can now simplify the view function, as the presentation of the page
+    # has been offloaded to the HTML template. Note the structure of this
+    # function: the first argument is the template name, index.html.
+    # the second and third arguments are required to fill in the 
+    # variables called out in the HTML file index.html.
+    # The render_template() function invokes the Jinja2 template
+    # engine that comes bundled with the Flask framework.
+    # Jinja2 substitutes {{ ... }} 
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
+# Chapter 1:
 # The @app.route decorator creates an association between the URL
 # given as an argument and the function.
 
@@ -27,3 +38,16 @@ def index():
 # top-level that defines the Flask application instance.
 # We will call the script microblog.py, and define it as a single
 # line that imports the application instance.
+
+# From Chapter 2:
+# Note that you can return raw HTML to the view, but this is not
+# considered sustainable or good practice.
+
+# If we can keep the logic of the application separate from the layout
+# or presentation of the web pages, this will be far better. It also
+# separates the front-end development and the back-end development
+# a bit more.
+
+# Templates help achieve this separation between the view logic and
+# the business logic. In Flask, templates are written as separate files,
+# stored in a templates folder that is inside the application package.
