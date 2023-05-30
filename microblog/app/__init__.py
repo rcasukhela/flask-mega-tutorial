5fe653555c0921924a35428eccf437de2e47cc38
Rohan Casukhela <43920866+rcasukhela@users.noreply.github.com>
2023-05-30 13:29:33 -0400

    Created __init__, and routes.py
    __init__.py is used to make the app/ dir a module, and is used for a starting point for loading resources for the rest of the application.
    
    First view function in routes.py.
    
    Keep in mind that Miguel is starting from an application structure that is more complicated than the basic hello world, but that can be used to build up more complex applications from there.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -13,3 +13,39 @@ app = Flask(__name__)
 
 from app import routes
 
+# The logic above, creates the application object,
+# as an instance of the class Flask, imported
+# from the flask package.
+
+# The variable __name__ is a Python 
+# pre-defined variable.
+
+# Flask uses the location of the module passed
+# here as a starting point when it needs
+# to load associated resources such as template files,
+# which will be covered in Chapter 2.
+# For practical purposes, passing __name__ will
+# usually work.
+
+# The application imports the module routes,
+# which doesn't exist yet.
+
+# Note that the module routes is imported
+# at the bottom and not at the top of the script,
+# as is usually done. This is a workaround to circular
+# imports, and I'll just leave it at that because
+# the rest of that explanation is confusing the hell
+# out of me.
+
+# The module routes contains the different URLs that
+# the application implements.
+
+# In Flask, handlers for the routes (or URLs) are written as Python
+# functions, called 'view functions'.
+
+# View functions are mapped to one or more route URLs
+# so that Flask knows what logic to execute when a client
+# requests a given URL.
+
+# We write the first view function for this application in
+# app/routes.py.
